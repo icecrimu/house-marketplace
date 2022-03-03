c5e2c55b531c844df9488052a2a21be0f6b31106
icecrimu <charles.banigon@gmail.com>
2022-03-04 00:16:02 +0800

    Listings page slider

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper"
+import "swiper/css"
+import "swiper/css/bundle"
+import { Swiper, SwiperSlide } from "swiper/react"
 import { getDoc, doc } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 import { db } from "../firebase.config"
 import Spinner from "../components/Spinner"
 import shareIcon from "../assets/svg/shareIcon.svg"
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 export default function Listing() {
   const [listing, setListing] = useState(null)
@@ -34,7 +39,21 @@ export default function Listing() {
 
   return (
     <main>
-      {/* SLIDER */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) 
+                center no-repeat`,
+                backgroundSize: "cover",
+                height: "250px"
+              }}
+              className="swiperSlideDiv"
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       <div
         className="shareIconDiv"
