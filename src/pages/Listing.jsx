1d6204e4754d0e317eafaf51e34ca2d47f625579
icecrimu <charles.banigon@gmail.com>
2022-03-03 20:40:19 +0800

    Fetch Single Listing

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { getDoc, doc } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+import { db } from "../firebase.config"
+import Spinner from "../components/Spinner"
+import shareIcon from "../assets/svg/shareIcon.svg"
+
+export default function Listing() {
+  const [listing, setListing] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [shareLinkCopied, setShareLinkCopied] = useState(false)
+
+  const navigate = useNavigate()
+  const params = useParams()
+  const auth = getAuth()
+
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId)
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        setListing(docSnap.data())
+        setLoading(false)
+      }
+    }
+
+    fetchListing()
+  }, [navigate, params.listingId])
+
+  return <div>Listing</div>
+}
